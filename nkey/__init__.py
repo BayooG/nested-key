e8901d2db19108b04edcc58a77a2634e963943e2
bayoog <obaydaba96@gmail.com>
2022-11-28 20:28:03 +0100

    chore: add docstring

--- nkey/__init__.py
+++ nkey/__init__.py
@@ -1,6 +1,15 @@
 
-def getNestedKeyValue(obj: dict, keys: list):
+def get_value(obj: dict, keys: list):
+    """gets a value from nested dict
+
+    Args:
+        obj (dict): nested dict
+        keys (list): list of nest keys
+
+    Returns:
+        _type_: _description_
+    """
     if len(keys) == 1:
         return obj.get(keys[0], None)
     else:
-        return getNestedKeyValue(obj.get(keys[0], {}), keys[1:])
+        return get_value(obj.get(keys[0], {}), keys[1:])
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 NAME = "nested-key"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 
 setup(
